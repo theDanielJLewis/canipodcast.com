358ae4b08887c93f9e0bfb416e546a2f72269ea6
Daniel J. Lewis <mail@theaudacitytopodcast.com>
2024-02-02 22:53:29 -0500

    Package upgrades

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	experimental: {
-		serverActions: true,
 		// esmExternals: 'loose',
 		// serverComponentsExternalPackages: ['activecampaign-sdk', 'api', 'ac'],
 	},
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,3 @@
-import Image from 'next/image';
-import { Input } from '@/components/ui/input';
-import { Button } from '@/components/ui/button';
 import { EmailForm } from '@/components/EmailForm';
 
 export default function Home() {
