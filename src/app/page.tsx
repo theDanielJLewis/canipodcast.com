6b42924af47e7c07b2deb49124f791aa4a17f8d9
Daniel J. Lewis <mail@theaudacitytopodcast.com>
2024-10-25 18:08:02 -0400

    Fixed headline size

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
 	return (
 		<main className="container flex max-w-lg flex-col justify-center gap-4">
 			<div>
-				<h1 className="text-orange-500">Can I Podcast?</h1>
+				<h1 className="text-4xl text-orange-500">Can I Podcast?</h1>
 				<p>
 					See and compare podcasting features across podcast apps and hosting
 					providers.
